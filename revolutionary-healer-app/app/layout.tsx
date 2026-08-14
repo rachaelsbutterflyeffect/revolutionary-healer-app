805bf24fdad57b23546a93252fa61b09965ad229
rachaelsbutterflyeffect <rachaelsbutterflyeffect@gmail.com>
2026-08-14 12:43:04 -0400

    Update layout.tsx

--- revolutionary-healer-app/app/layout.tsx
+++ revolutionary-healer-app/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="/bg.css" />
+      </head>
       <body>{children}</body>
     </html>
   );
